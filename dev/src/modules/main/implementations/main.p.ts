9da4f00edc4678dec33974f797d7b3e97396836b
corno <corno.astn.a@schraverus.com>
2023-02-24 19:29:29 +0100

    .

--- dev/src/modules/main/implementations/main.p.ts
+++ dev/src/modules/main/implementations/main.p.ts
@@ -1,24 +1,32 @@
-import * as pd from 'pareto-core-dev'
+import * as pv from 'pareto-core-dev'
+import * as pd from 'pareto-core-data'
 
 import * as gpareto from "lib-pareto-typescript-project"
-//import * as gliana2pareto from "lib-pareto-typescript-project/dist/submodules/liana2pareto"
+import * as gliana2pareto from "lib-pareto-typescript-project/dist/submodules/liana2pareto"
 
 import { $ as glossary } from "../../../data/glossary.data"
 
 import { Cmain } from "../api"
 
-export const $$:Cmain = ($) => {
-    pd.logDebugMessage("REENABLE LIANA GENERATION")
-    // mliana2pareto.$a.generateProject({
-    //     'mainData': {
-    //         'arguments': pr.wrapRawArray([`../../pareto/src/data/glossary`]),
-    //     },
-    //     'model': {
-    //         'model': glossary,
+export const $$: Cmain = ($) => {
+    gliana2pareto.$a.generateModule({
+        'path': `../../pareto/src/data/glossary`,
+        'data': {
+            'configuration': {
+                'datamodel': [true, {
+                    'annotations': true,
+                    'properties optional': false,
+                    'reference mapping': ['string', {}],
+                }],
+                'algorithms': {},
+            },
+            'mappedModel': {
+                'model': glossary,
 
-    //         'stringmapping': pd.wrapRawDictionary({
-    //             "identifier": ['string', null]
-    //         }),
-    //     },
-    // })
+                'stringmapping': pd.d({
+                    "identifier": ['string', null]
+                }),
+            },
+        }
+    })
 }

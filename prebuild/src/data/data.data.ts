bbcffd220358a482b6ec176d59ee02ae9ea2a708
corno <corno.astn.a@schraverus.com>
2023-03-07 22:06:53 +0100

    .

--- prebuild/src/data/data.data.ts
+++ prebuild/src/data/data.data.ts
@@ -1,52 +1,53 @@
 import * as pd from 'pareto-core-data'
-import * as pt from 'pareto-core-types'
 
-import * as gliana2glossary from "lib-liana/dist/submodules/liana2glossary"
+import * as gliana from "lib-liana/dist/main"
 
 import { $ as glossary } from "./models/glossary.data"
 
-export const $: pt.Array<gliana2glossary.T.GenerateData<pd.SourceLocation>> = pd.a([
-    {
-        'path': `../../pareto/src/data/submodules/glossary/glossary.generated.ts`,
-        'data': {
-            'settings': {
-                'datamodel': [true, {
-                    'annotations': true,
-                    'properties optional': false,
-                    'reference mapping': ['string', null],
-                }],
-                'visitor interface': [false],
-                'algorithms': {
-                    'serialize': [false],
+export const $: gliana.T.CompileParameters<pd.SourceLocation> = {
+    'outputs': pd.a([
+        {
+            'path': `../../pareto/src/data/submodules/glossary/glossary.generated.ts`,
+            'data': {
+                'settings': {
+                    'datamodel': [true, {
+                        'annotations': true,
+                        'properties optional': false,
+                        'reference mapping': ['string', null],
+                    }],
+                    'visitor interface': [false],
+                    'algorithms': {
+                        'serialize': [false],
+                    },
                 },
-            },
-            'mapped model': {
-                'model': glossary,
+                'mapped model': {
+                    'model': glossary,
 
-                'terminal mapping': pd.d({
-                    "identifier": ['string', null],
-                    "boolean": ['boolean', null],
-                }),
-            },
-        }
-    },
-    // {
-    //     'path': `../../pareto/src/data/submodules/glossary_serialize`,
-    //     'data': {
-    //         'configuration': {
-    //             'datamodel': [false],
-    //             'visitor interface': [false],
-    //             'algorithms': {
-    //                 'serialize': [true, null],
-    //             },
-    //         },
-    //         'mappedModel': {
-    //             'model': glossary,
+                    'terminal mapping': pd.d({
+                        "identifier": ['string', null],
+                        "boolean": ['boolean', null],
+                    }),
+                },
+            }
+        },
+        // {
+        //     'path': `../../pareto/src/data/submodules/glossary_serialize`,
+        //     'data': {
+        //         'configuration': {
+        //             'datamodel': [false],
+        //             'visitor interface': [false],
+        //             'algorithms': {
+        //                 'serialize': [true, null],
+        //             },
+        //         },
+        //         'mappedModel': {
+        //             'model': glossary,
 
-    //             'stringmapping': pd.d({
-    //                 "identifier": ['string', null]
-    //             }),
-    //         },
-    //     }
-    // }
-])
+        //             'stringmapping': pd.d({
+        //                 "identifier": ['string', null]
+        //             }),
+        //         },
+        //     }
+        // }
+    ])
+}
--- prebuild/src/modules/main/implementations/main.p.ts
+++ prebuild/src/modules/main/implementations/main.p.ts
@@ -1,11 +1,17 @@
-import * as gliana2glossary from "lib-liana/dist/submodules/liana2glossary"
+import * as pv from 'pareto-core-dev'
+import * as pd from 'pareto-core-data'
+
+import * as gliana2pareto from "lib-liana/dist/submodules/liana2glossary"
+import * as gresolve from "lib-liana/dist/submodules/resolve"
+import * as gerror from "lib-liana/dist/submodules/errorMessaging"
+import * as gmain from "lib-liana/dist/main"
 
 import { $ as data } from "../../../data/data.data"
 
 import { main } from "../api"
 
 export const $$: main = ($) => {
-    data.__forEach(($) => {
-        gliana2glossary.$a.generate($)
+    gmain.$a.compile(data, ($) => {
+        pv.logDebugMessage(gerror($))
     })
 }
